a2b86c98320db7577e28fc6dea951a9fa80d37f1
GuillermoMajano <jmajano95@gmail.com>
2023-02-26 19:50:20 -0400

    modified:   server/go.mod 	modified:   server/go.sum 	modified:   server/router/router.go

--- server/go.mod
+++ server/go.mod
@@ -2,4 +2,7 @@ module github.com/GuillermoMajano/todo-app
 
 go 1.18
 
-require go.mongodb.org/mongo-driver v1.11.2
+require (
+	github.com/gorilla/mux v1.8.0
+	go.mongodb.org/mongo-driver v1.11.2
+)
--- server/router/router.go
+++ server/router/router.go
@@ -1,5 +1,21 @@
 package router
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func NewRouter() http.Handler
+
+func Router() *mux.Router {
+	r := mux.NewRouter()
+
+	r.HandleFunc("api/task", func(http.ResponseWriter, *http.Request) {}).Methods("GET", "OPTIONS")
+	r.HandleFunc("api/tasks", func(http.ResponseWriter, *http.Request) {}).Methods("POST", "OPTIONS")
+	r.HandleFunc("api/tasks/{id}", func(http.ResponseWriter, *http.Request) {}).Methods("PUT", "OPTIONS")
+	r.HandleFunc("api/undoTask/{id}", func(http.ResponseWriter, *http.Request) {}).Methods("PUT", "OPTIONS")
+	r.HandleFunc("api/delateTasks", func(http.ResponseWriter, *http.Request) {}).Methods("DELETE", "OPTIONS")
+	r.HandleFunc("api/delateAllTasks", func(http.ResponseWriter, *http.Request) {}).Methods("DELETE", "OPTIONS")
+	return r
+}
